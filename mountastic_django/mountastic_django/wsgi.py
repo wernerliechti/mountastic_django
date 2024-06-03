e3f6fdb61a05ef8f4b7e03249e26ae2af26c87a9
Werner Liechti <94870762+wernerliechti@users.noreply.github.com>
2024-06-03 22:41:37 +0200

    Updated Apache2 Files

--- mountastic_django/mountastic_django/wsgi.py
+++ mountastic_django/mountastic_django/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mountastic_django.settings')
 
+sys.path.append('/home/ubuntu/mountastic/mountastic-api/mountastic_django')
+sys.path.append('/home/ubuntu/mountastic/mountastic-api')
+
 application = get_wsgi_application()
